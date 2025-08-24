8aded95c6e1b102da3ddc862a03c518213075826
darren-stokes <darrenstokes2@hotmail.com>
2025-08-24 12:31:13 +0200

    Move the style to a dedicated CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Footer from './Footer.jsx';
+import './App.css';
 
 export default function App() {
   return (
-    <main style={{ textAlign: 'center', padding: '24px', fontFamily: 'system-ui,Avenir,Helvetica,Arial,sans-serif', color: '#ffffffde', fontWeight: '400' }}>
+    <main>
       <h1>Welcome to the React QR Code App 🚀</h1>
       <p>This app generates QR codes for various purposes.</p>
       <p>Running on K8s, served by Caddy.</p>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer style={{ textAlign: 'center', padding: '24px', fontFamily: 'system-ui,Avenir,Helvetica,Arial,sans-serif', color: '#ffffffde', fontWeight: '400' }}>
+        <footer>
             <p>
                 &copy; {new Date().getFullYear()} React QR Code App. All rights reserved.
                 {' '}<a href="http://hello.127.0.0.1.nip.io/" target="_blank" rel="noreferrer">Docs</a>
